7614d72da07e033ea09b0315f1de19080e1734f5
BerislavV <berislav.vidakovic@gmail.com>
2026-02-02 17:29:31 +0100

    ViewModel state rendering in UI razor Page

--- Frontend/ViewModels/HealthViewModel.cs
+++ Frontend/ViewModels/HealthViewModel.cs
@@ -3,9 +3,19 @@ using Shared.DTOs;
 
 namespace Frontend.ViewModels;
 
+/*
+MVVM Architecture
+
+-ViewModel owns UI state
+-It decides what message to show
+-UI just renders whatever ViewModel exposes
+*/
+
 public class HealthViewModel
 {
     private readonly HealthApiService _service;
+
+    public string StatusMessage { get; private set; } = "Checking backend...";
 
     public HealthResponseDto? Health { get; private set; }
 
@@ -17,8 +27,8 @@ public class HealthViewModel
     public async Task LoadHealthAsync()
     {
         Health = await _service.GetHealthAsync();
-        Console.WriteLine(Health != null
-            ? $"Connected to backend! Backend is running on https://localhost:5206"
-            : "Failed to connect to backend");
+        StatusMessage = Health != null
+            ? $"...Connected to backend! <br />Backend is running on https://localhost:5206"
+            : "Failed to connect to backend";
     }
 }
