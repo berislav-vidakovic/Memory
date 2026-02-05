37bc4b1b3512f8ba057d6de125f82cc6ba26383d
BerislavV <berislav.vidakovic@gmail.com>
2026-02-05 17:19:20 +0100

    Add Dummy Login dialog

--- Frontend/ViewModels/UsersViewModel.cs
+++ Frontend/ViewModels/UsersViewModel.cs
@@ -18,6 +18,11 @@ public class UsersViewModel
 
     public List<UsersResponseDto> AllUsers { get; private set; } = new();
 
+    public bool ShowLoginDialog { get; private set; }
+
+    public string? SelectedUserFullName { get; set; }
+    public string Password { get; set; } = string.Empty;
+
 
     public event Action? OnStateChanged;
 
@@ -31,10 +36,36 @@ public class UsersViewModel
     public async Task LoadUsersAsync()
     {
         AllUsers = await _userService.GetUsersAsync();    
- 
-
-
         OnStateChanged?.Invoke();
+    }
+
+    public List<UsersResponseDto> OfflineUsers =>
+        AllUsers.Where(u => !u.IsOnline).ToList();
+
+    public void OpenLoginDialog()
+    {
+        ShowLoginDialog = true;
+        OnStateChanged?.Invoke();
+    }
+
+    public void CloseLoginDialog()
+    {
+        ShowLoginDialog = false;
+        Password = string.Empty;
+        SelectedUserFullName = null;
+        OnStateChanged?.Invoke();
+    }
+
+    public void LoginOk()
+    {
+        Console.WriteLine($"LOGIN OK: {SelectedUserFullName}, Password: {Password}");
+        CloseLoginDialog();
+    }
+
+    public void LoginCancel()
+    {
+        Console.WriteLine("LOGIN CANCELLED");
+        CloseLoginDialog();
     }
 
 }
