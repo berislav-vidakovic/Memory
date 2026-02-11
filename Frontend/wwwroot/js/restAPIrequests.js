c18b487ed5592e0363131644b0072bd48c1ec707
BerislavV <berislav.vidakovic@gmail.com>
2026-02-11 15:10:45 +0100

    Add JS script

---
+++ Frontend/wwwroot/js/restAPIrequests.js
@@ -0,0 +1,8 @@
+﻿window.sendRequestPOST = async function (url, body) {
+    return await fetch(url, {
+        method: "POST",
+        credentials: "include",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body)
+    });
+};
